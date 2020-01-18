7e486d19a408ee8f38cf6cb5e8d63e7ed6c434a7
Allan Lin <allanlin97@gmail.com>
2020-01-18 18:10:05 -0500

    #6: add client collection

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,15 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Client;
+use App\Http\Resources\ClientResource;
+use App\Http\Resources\ClientResourceCollection;
 use Illuminate\Http\Request;
 
 
 class ClientController extends Controller
 {
-   public function show(Client $client){
+    //return an array resource 
+   public function show(Client $client): ClientResource{
 
-       return $client;
+       return new ClientResource($client);
 
+   }
+
+   //return all client in database
+   public function index() : ClientResourceCollection{
+
+       return new ClientResourceCollection(Client::paginate());
    }
 
 }
