cfad32df814009b337a57e655b378fb30d85ddac
Allan Lin <allanlin97@gmail.com>
2020-01-18 18:35:39 -0500

    #6 store client into db

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
-    //return an array resource 
+    //return an array resource
    public function show(Client $client): ClientResource{
 
        return new ClientResource($client);
@@ -22,5 +22,23 @@ class ClientController extends Controller
 
        return new ClientResourceCollection(Client::paginate());
    }
+
+    public function store(Request $request) {
+
+        $request->validate([
+            'client_name' => 'required',
+            'client_phone' => 'required',
+            'client_address' => 'required',
+            'client_email' => 'required',
+
+
+        ]);
+
+        // creates a building once pass validation
+        $client = Client::create($request->all());
+
+        return new ClientResource($client);
+
+    }
 
 }
